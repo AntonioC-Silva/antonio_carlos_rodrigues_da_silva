1515ff9bab7995c9de4ffae679f497180a6bbcb1
ANTÔNIO CARLOS RODRIGUES DA SILVA <antonio.silva110@senaisp.edu.br>
2026-04-15 16:23:13 -0300

    front

--- Front/src/components/sobreMim/sobreMim.jsx
+++ Front/src/components/sobreMim/sobreMim.jsx
@@ -10,7 +10,7 @@ const SobreMim = () => {
     >
       <figure className="areaFoto">
         <img 
-          src="https://media.licdn.com/dms/image/v2/D4D22AQHbF0Wd0jqnUw/feedshare-shrink_800/B4DZgzz0AHGkAg-/0/1753215877565?e=1766620800&v=beta&t=eRWXqInlmGMolRUSaSqRWyj95JoH7YqoZeEaA-r5dZI" 
+          src="./eu.jpg" 
           alt="Desenvolvedor Full Stack Antônio Rodrigues"
           className="sobreFoto"
         />
